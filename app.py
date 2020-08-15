f5f58ab38ddcb1b897797861405b1fc3ac999495
climbr88 <50963894+climbr88@users.noreply.github.com>
2020-08-14 22:14:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,28 @@
+# ## Mission To Mars
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import scrape_surfing
+import scrape_mars
 
-
+# Setup Flask App
 app = Flask(__name__)
 
+mongo = PyMongo(app, uri = 'mongodb://localhost:27017')
 
-mongo = PyMongo(app, uri="mongodb://localhost:27017/surfing_app")
-
-
-@app.route('/')
+@app.route("/")
 def index():
-    surfing = mongo.db.surfing.find_one()
-    return render_template('index.html', surfing=surfing)
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
 
-
-@app.route('/scrape')
+@app.route("/scrape")
 def scrape():
-    surfing = mongo.db.surfing
-    data = scrape_surfing.scrape()
-    surfing.update(
+    mars= mongo.db.mars
+    data = scraper.scrape()
+    mars.update(
         {},
         data,
         upsert=True
     )
-    return redirect("/", code=302)
-
+    return redirect("http://localhost:5000/", code=302)
 
 if __name__ == "__main__":
     app.run(debug=True)
